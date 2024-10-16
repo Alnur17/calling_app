c2d52da676f570eda492bf36b082cf84bcba8982
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2024-10-16 11:03:05 +0600

    init
    separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
 
+import 'package:calling_app/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,26 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AudioVideoCall(),
+      home: const HomeScreen(),
     );
   }
 }
 
-
-class AudioVideoCall extends StatelessWidget {
-  AudioVideoCall({super.key});
-
-  final String userID = Random().nextInt(1000).toString();
-
-  @override
-  Widget build(BuildContext context) {
-    return ZegoUIKitPrebuiltCall(
-        appID: 1175988419,
-        appSign:
-        '3b5e157515193cf9ff723175590d972db363e95180220c61712ac637dab05059',
-        callID: 'id_123',
-        userID: 'user_$userID',
-        userName: 'user_$userID',
-        config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall());
-  }
-}
